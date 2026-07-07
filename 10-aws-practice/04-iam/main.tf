3406476d8d84bb75bf5cb5decfa418661657fe31
krishnakala987-byte <krishnakala987@gmail.com>
2026-07-07 23:50:42 +0530

    Add IAM user, group and policy attachment resources

--- 10-aws-practice/04-iam/main.tf
+++ 10-aws-practice/04-iam/main.tf
@@ -11,3 +11,21 @@ provider "aws" {
     region = "us-east-1"
 }
 
+
+resource "aws_iam_user" "demo_user" {
+    name = "terraform-demo-user"
+}
+
+resource "aws_iam_group" "demo_group" {
+    name = "terraform-demo-group"
+}
+
+resource "aws_iam_user_group_membership" "demo_membership" {
+    user   = aws_iam_user.demo_user.name
+    groups = [aws_iam_group.demo_group.name]
+}
+
+resource "aws_iam_group_policy_attachment" "s3_read_only" {
+    group      = aws_iam_group.demo_group.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
